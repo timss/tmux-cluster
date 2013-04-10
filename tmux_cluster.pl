9bfb18dbb55c72eb46670eb60dae0996a09695e9
timss <timsateroy@gmail.com>
2013-04-10 02:55:51 +0200

    Old (but working) tmux_cluster written in perl for simple usage.

---
+++ tmux_cluster.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+#---TMUX-CLUSTER---#
+#    Version: v1   #
+#------------------#
+
+use strict;
+
+if($#ARGV < 0) {
+    print "Usage: $0 application.\n";
+    print "Applications available: Foo, Bar.\n";
+    exit 0;
+}
+
+my $session = lc($ARGV[0]);
+my $applications = {foo = ["a", "b", "c"],
+                    bar = ["d", "e", "f"]};
+
+system("tmux new-window -n $session");
+
+foreach my $server (@{$applications->{$session}}) {
+    system("tmux split-window -v \"ssh $server\"");
+    system("tmux select-layout tiled");
+}
+
+system("tmux kill-pane -t 0");
+exec("tmux set-window-option synchronize-panes on"); # exec terminates this script
